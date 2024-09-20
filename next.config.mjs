5ccd19c2a2d72e3233b58197e249e01279203c09
MAPCMC <maartenadampeene@gmail.com>
2024-09-20 15:34:59 +0200

    Setup typesafe env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import .env to validate during build
+jiti("./src/env/server.ts");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+};
 
 export default nextConfig;
---
+++ src/env/server.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z, ZodError } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production"]),
+  },
+  onValidationError: (error: ZodError) => {
+    console.error(
+      "❌ Invalid environment variables:",
+      error.flatten().fieldErrors
+    );
+    process.exit(1);
+  },
+  emptyStringAsUndefined: true,
+  experimental__runtimeEnv: process.env,
+});
